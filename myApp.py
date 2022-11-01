6fe602c77a0d14174d4fb487c7cb83eb2b36157a
Christian <c98e@outlook.de>
2022-11-01 13:04:18 +0100

    optimize plot

--- myApp.py
+++ myApp.py
@@ -36,11 +36,13 @@ role_dict = {
 
 st.title('Kickbase Analyzer')
 
+kb = Kickbase()
+user_me, league = kb.login(st.secrets.kickbase_credentials.username, st.secrets.kickbase_credentials.password)
+
 DB_FILE = "database/player.db"
 
-def update_database(username, password):
-    kb = Kickbase()
-    user, league = kb.login(username, password)
+def update_database():
+    
 
     conn = sqlite3.connect(DB_FILE)
 
@@ -59,23 +61,6 @@ def update_database(username, password):
                     (market_value, market_value_trend, status, role_dict[0], player_id)
                     )
         
-
-        users = kb.league_users(kb._get_league_id(league[0]))
-        for user in users:
-            players = kb.league_user_players(league[0], user)
-            for i, p in enumerate(players):
-                player_id = kb._get_player_id(p)
-                if(i == 0):
-                    cur.execute(
-                        "UPDATE spieler SET role = ? WHERE player_id = ?;", 
-                        (role_dict[2], player_id)
-                        )
-                else:
-                    cur.execute(
-                        "UPDATE spieler SET role = ? WHERE player_id = ?;", 
-                        (role_dict[1], player_id)
-                        )
-
         market = kb.market(league[0])
         market_players = market.players
         for p in market_players:
@@ -84,6 +69,27 @@ def update_database(username, password):
                 "UPDATE spieler SET role = ? WHERE player_id = ?;",
                 (role_dict[3], player_id)
                 )
+
+        users = kb.league_users(kb._get_league_id(league[0]))
+        for user in users:
+            print(user.name)
+            print(user.id)
+            print(user_me.id)
+            players = kb.league_user_players(league[0], user.id)
+            for p in players:
+                player_id = kb._get_player_id(p)
+                if(user.id == user_me.id):
+                    cur.execute(
+                        "UPDATE spieler SET role = ? WHERE player_id = ?;", 
+                        (role_dict[1], player_id)
+                        )
+                else:
+                    cur.execute(
+                        "UPDATE spieler SET role = ? WHERE player_id = ?;", 
+                        (role_dict[2], player_id)
+                        )
+
+        
 
 
 
@@ -97,7 +103,7 @@ def construct_query(positions):
 
 
 # DataFrame (Pandas) holt sich Daten aus sqlite-Datenbank
-@st.cache
+#@st.cache
 def load_data(next_matchday, avg_range, positions, delete_peaks):
     conn = sqlite3.connect(DB_FILE)
 
@@ -128,9 +134,11 @@ def load_data(next_matchday, avg_range, positions, delete_peaks):
     print(df)
     return df
 
-st.write("## Average Points")
-avg_range = st.slider("Current matchday", 1, 34, 11)
-avg_range = st.slider("Select how many matchdays will count", 1, avg_range, 5)
+
+match_day = kb.league_stats(kb._get_league_id(league[0])).current_day
+st.write(f"### {str(match_day)}. Matchday")
+
+avg_range = st.slider("Select how many matchdays will count", 1, match_day, 5)
 
 positions = st.multiselect(
     'Positions to show',
@@ -140,22 +148,34 @@ positions = st.multiselect(
 delete_peaks = st.checkbox('Delete peaks')
 
 data_load_state = st.text('Loading data...')
-df = load_data(11, avg_range, positions, delete_peaks)
+df = load_data(match_day, avg_range, positions, delete_peaks)
 data_load_state.text("Done! (using st.cache)")
 
 if st.button('Update Database'):
-    update_database(st.secrets.kickbase_credentials.username, st.secrets.kickbase_credentials.password)
+    update_database()
 
 if st.checkbox('Show raw data'):
     st.subheader('Raw data')
     st.write(df)
 
-st.subheader('Number of pickups by hour')
-fig = px.scatter(
-    df, 
-    x="avg_points", 
-    y="value", 
+st.subheader(f'Average Points of last {avg_range} matchdays')
+fig = px.scatter(df, x="avg_points", y="value",
+    labels={
+        "avg_points": "Average Points",
+        "value": "Market Value",
+        "role": "Role"
+    },
     color="role",
-    hover_data=["last_name", "first_name",  "position", "status", "team"]
+    custom_data=["last_name", "first_name", "position", "status", "team"]
     )
+fig.update_traces(
+    hovertemplate="<br>".join([
+        "<b>%{customdata[0]}</b> (%{customdata[2]})",
+        "%{customdata[1]}",
+        "<i>%{customdata[3]}</i>",
+        "%{customdata[4]}",
+        "<b>%{y}</b>"
+        ])
+    )
+
 st.plotly_chart(fig, use_container_width=True)
