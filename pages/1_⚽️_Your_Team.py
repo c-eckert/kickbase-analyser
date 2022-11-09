f509da3ab13ec049ac141a4b26d1e866b4c86571
Christian <c98e@outlook.de>
2022-11-09 16:22:19 +0100

    frontend

--- pages/1_⚽️_Your_Team.py
+++ pages/1_⚽️_Your_Team.py
@@ -11,7 +11,7 @@ def main():
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
     st.markdown("# Your Team")
-    st.sidebar.header("Summe")
+    st.sidebar.subheader("Team value")
 
     kb, league_id = myKickbase.get_kickbase_object()
     users = kb.league_users(league_id)
@@ -25,22 +25,22 @@ def main():
     user_players = kb.league_user_players(league_id, user_dict[selcted_user])
     counting_value = [0]*len(user_players)
     values = [int(p.market_value) for p in user_players]
+    st.sidebar.text(f"{sum(values):,} €")
     
     for position in myKickbase.POSITION_DICT:
-        st.markdown(f"## {myKickbase.POSITION_DICT[position]}")
-        st.markdown("----")
+        st.subheader(myKickbase.POSITION_DICT[position])
         for i, user_player in enumerate(user_players):
             if user_player.position == position:
                 btn = st.checkbox("Sell", key=i)
                 if btn:
                     counting_value[i] = 1
                 st.markdown(f"""
-                    <table>
-                        <tr>
-                            <td width="30%">
+                    <table style="padding: 1%;">
+                        <tr style="border: 0px;">
+                            <td width="30%" style="border: 0px; padding: 0px;">
                                 <img src={user_player.profile_big_path}>
                             </td>
-                            <td width="70%">
+                            <td width="70%" style="border: 0px; padding: 0px;">
                                 <div class="number">{user_player.number} {myKickbase.POSITION_DICT[user_player.position]}</div>
                                 <div class="last_name">{user_player.last_name}</div>
                                 <div class="first_name">{user_player.first_name}</div>
@@ -59,6 +59,8 @@ def main():
                     </table>
                     <div height="10"></div>
                 """, unsafe_allow_html=True)
+    
+    st.sidebar.subheader("Value of selected Players")
     sum_values = sum([a*b for a,b in zip(counting_value,values)])
     st.sidebar.text(f"{sum_values:,} €")
 if __name__ == "__main__":
